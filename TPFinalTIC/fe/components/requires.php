b73f68c92f4cd6da01242277ec5e585631fca73d
Patricio99 <patonaveira@gmail.com>
2017-11-26 23:39:35 -0300

    mobileresposive

--- TPFinalTIC/fe/components/requires.php
+++ TPFinalTIC/fe/components/requires.php
@@ -4,5 +4,7 @@
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css"/>
 <link rel="stylesheet" href="node_modules\tether\dist\css\tether.css"/>
